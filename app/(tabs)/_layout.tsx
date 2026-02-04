730ee1724f7c1db8fa46f07ec8e072b823a21643
roosmagi <angela.roosmagi@voco.ee>
2026-02-04 20:53:49 +0200

    Tab navigation and Homepage

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,74 @@
+import { Image } from 'expo-image';
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#4F63AC',
+        tabBarInactiveTintColor: '#999999',
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          padding: 3,
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        tabBarItemStyle: {
+          flex: 0,
+          width: 65,
+          marginHorizontal: 8,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Image
+              source={
+                focused
+                  ? require('@/assets/images/home-solid.svg')
+                  : require('@/assets/images/home.svg')
+              }
+              style={{ width: 24, height: 24, tintColor: color }}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favorites"
+        options={{
+          tabBarLabel: 'Favorites',
+          tabBarIcon: ({ color, focused }) => (
+            <Image
+              source={
+                focused
+                  ? require('@/assets/images/favorites-solid.svg')
+                  : require('@/assets/images/favorites.svg')
+              }
+              style={{ width: focused ? 24 : 16, height: focused ? 24 : 20, tintColor: color }}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color, focused }) => (
+            <Image
+              source={
+                focused
+                  ? require('@/assets/images/profile-solid.svg')
+                  : require('@/assets/images/profile.svg')
+              }
+              style={{ width: focused ? 24 : 24, height: focused ? 24 : 24, tintColor: color }}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,100 @@
+import { CategoryBox } from '@/components/CategoryBox';
+import { ProductHomeItem } from '@/components/ProductHomeItem';
+import { categories } from '@/data/categories';
+import { products } from '@/data/products';
+import { Image } from 'expo-image';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  const [selectedCategoryId, setSelectedCategoryId] = useState<number | null>(null);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          source={require('@/assets/images/search.svg')}
+          style={styles.searchIcon}
+        />
+        <Text style={styles.headerText}>Find All You Need</Text>
+      </View>
+
+      <FlatList
+        data={categories}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={styles.categoryList}
+        contentContainerStyle={styles.listContent}
+        keyExtractor={(item, index) => String(item.id ?? `popular-${index}`)}
+        ItemSeparatorComponent={() => <View style={styles.listSeparator} />}
+        renderItem={({ item }) => (
+          <CategoryBox
+            item={item}
+            selected={(item.id ?? null) === selectedCategoryId}
+            onPress={() => setSelectedCategoryId(item.id ?? null)}
+          />
+        )}
+      />
+
+      <FlatList
+      data={products}
+      numColumns={2}
+      columnWrapperStyle={styles.row}
+      contentContainerStyle={styles.content}
+      keyExtractor={(item) => String(item.id)}
+      renderItem={({ item }) => <ProductHomeItem item={item} />}
+      ListFooterComponent={<View style={{ height: 24 }} />}
+      showsVerticalScrollIndicator={false}
+    />  
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 18,
+    backgroundColor: '#fff',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 16,
+    backgroundColor: '#fff',
+    gap: 16,
+  },
+  searchIcon: {
+    width: 24,
+    height: 24,
+    tintColor: '#4F63AC',
+  },
+  headerText: {
+    flex: 1,
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#303030',
+  },
+  listContent: {
+    paddingHorizontal: 16,
+    marginTop: 16,
+    paddingBottom: 10,
+  },
+  listSeparator: {
+    width: 20,
+  },
+  categoryList: {
+    marginBottom: 12,
+  },
+  row: {
+    paddingHorizontal: 16,
+    justifyContent: "space-between",
+  },
+  content: {
+    paddingTop: 8,
+    backgroundColor: '#fff',
+  },
+
+});
